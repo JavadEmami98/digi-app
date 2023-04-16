c0e58c55f0a22174373fe9e1c4a455c90842fd20
JavadEmami98 <javademami77@gmail.com>
2023-04-17 03:12:36 +0430

    Add all my files

--- src/page/Index.jsx
+++ src/page/Index.jsx
@@ -5,7 +5,73 @@ import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 //import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import StoreIcon from "@mui/icons-material/Store";
+import WhatshotOutlinedIcon from "@mui/icons-material/WhatshotOutlined";
+import PercentOutlinedIcon from "@mui/icons-material/PercentOutlined";
+import LocalOfferOutlinedIcon from "@mui/icons-material/LocalOfferOutlined";
+import { Link } from "react-router-dom";
 import "./first.css";
+
+
+const CATEGROY = [
+  {
+    title: "هندزفری",
+    image: "https://api.tahlilmobile.ir/public/downloadFileById/146",
+    id: 0,
+  },
+  {
+    title: "اپل واچ",
+    image: "https://api.tahlilmobile.ir/public/downloadFileById/60",
+    id: 1,
+  },
+  {
+    title: "آیفون",
+    image: "https://api.tahlilmobile.ir/public/downloadFileById/1800",
+    id: 2,
+  },
+  {
+    title: "کنسول بازی",
+    image: "https://api.tahlilmobile.ir/public/downloadFileById/148",
+    id: 3,
+  },
+  {
+    title: "تبلت",
+    image: "https://api.tahlilmobile.ir/public/downloadFileById/1797",
+    id: 4,
+  },
+  {
+    title: "قاب",
+    image: "https://api.tahlilmobile.ir/public/downloadFileById/152",
+    id: 5,
+  },
+  {
+    title: "لوازم جانبی",
+    image: "https://api.tahlilmobile.ir/public/downloadFileById/63",
+    id: 6,
+  },
+  {
+    title: " هوم پاد",
+    image: "https://api.tahlilmobile.ir/public/downloadFileById/147",
+    id: 7,
+  },
+  {
+    title: " پاور بانک",
+    image: "https://api.tahlilmobile.ir/public/downloadFileById/151",
+    id: 8,
+  },
+  {
+    title: " لوازم خانگی",
+    image: "https://api.tahlilmobile.ir/public/downloadFileById/153",
+    id: 9,
+  },
+  {
+    title: " نوت بوک",
+    image: "https://api.tahlilmobile.ir/public/downloadFileById/149",
+    id: 10,
+  },
+];
+
 function Index() {
   return (
     <div>
@@ -17,7 +83,27 @@ function Index() {
       />
       <div className="logo-line">
         <img className="logo" src="/image/logo.svg" alt="" loading="lazy" />
-        <input className="input" type="text" placeholder="جستجو" />
+        <Box
+          sx={{
+            display: "flex",
+            width: "600px",
+            height: "44px",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <SearchIcon
+            sx={{
+              position: "absolute",
+              right: "170px",
+              top: "22px",
+              zIndex: 10,
+              color: "#9e9fb1",
+            }}
+          />
+          <input className="input" type="text" placeholder="جستجو" />
+        </Box>
+
         <div className="head-left">
           <Box
             sx={{
@@ -43,10 +129,12 @@ function Index() {
         sx={{
           display: "flex",
           justifyContent: "space-between",
+          flexDirection: "row",
           alignItems: "center",
           width: "100%",
           height: "40px",
           fontSize: "12px",
+          borderBottom: "1px solid #5a5c7a",
         }}
       >
         <Box
@@ -62,6 +150,98 @@ function Index() {
           <MenuOutlinedIcon sx={{ mr: "15px" }} />
           <Typography sx={{ ml: "10px" }}>دسته‌بندی کالاها</Typography>
         </Box>
+        |
+        <Box
+          sx={{
+            display: "flex",
+            /* justifyContent: "space-between", */
+            alignItems: "center",
+            ml: "595px",
+            width: "770px",
+            height: "40px",
+           
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "110px",
+              height: "40px",
+           
+            }}
+          >
+            <StoreIcon
+              sx={{ mr: "10px", paddingRight: "5px", color: "#5a5c7a" }}
+            />
+            <Typography sx={{ fontSize: "12px", pl: "15px", color: "#5a5c7a" }}>
+              سوپرمارکت
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "115px",
+              height: "40px",
+            }}
+          >
+            <WhatshotOutlinedIcon
+              sx={{ mr: "8px", paddingRight: "5px", color: "#5a5c7a" }}
+            />
+            <Typography sx={{ fontSize: "10px", pl: "15px", color: "#5a5c7a" }}>
+              پرفروش ترینها
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "125px",
+              height: "40px",
+          
+            }}
+          >
+            <PercentOutlinedIcon
+              sx={{ mr: "8px", paddingRight: "5px", color: "#5a5c7a" }}
+            />
+            <Typography sx={{ fontSize: "9px", pl: "15px", color: "#5a5c7a" }}>
+              تخفیف‌ها و پیشنهادها
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "110px",
+              height: "40px",
+            }}
+          >
+            <LocalOfferOutlinedIcon
+              sx={{ mr: "10px", paddingRight: "5px", color: "#5a5c7a" }}
+            />
+            <Typography sx={{ fontSize: "10px", pl: "15px", color: "#5a5c7a" }}>
+              شگفت‌انگیزها
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              mr: "7px",
+            }}
+          >
+            |
+          </Box>
+          <Box sx={{ marginRight: "15px", color: "#5a5c7a" }}>
+            <Link>سوالی دارید؟ </Link>
+          </Box>
+          <Typography sx={{ fontSize: "12px", color: "#5a5c7a", mr: "25px" }}>
+            در اینجا بفروشید!
+          </Typography>
+        </Box>
         <Box
           sx={{
             display: "flex",
@@ -75,11 +255,44 @@ function Index() {
             paddingLeft: "5px",
           }}
         >
-          <LocationOnOutlinedIcon />
-          <Typography sx={{ fontSize: "12px" }}>
+          <LocationOnOutlinedIcon
+            sx={{ pr: "4px", mr: "8px", color: "#5a5c7a" }}
+          />
+          <Typography sx={{ fontSize: "11px", color: "#5a5c7a" }}>
             لطفا شهر خود را انتخاب کنید
           </Typography>
         </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          width: "94%",
+          height: "150px",
+          margin: "5px 50px",
+        }}
+      >
+        {CATEGROY.map((item, index) => (
+          <Box
+            key={index}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexDirection: "column",
+              width: "105px",
+              height: "115px",
+              margin: "20px 20px",
+              paddingTop: "5px",
+              paddingBottom: "5px",
+              fontWeight: "light",
+            }}
+          >
+            <img className="smalls-pic" src={item.image} alt="" />
+            <Typography sx={{ fontSize: "11px", fontWeight: "light" }}>
+              {item.title}
+            </Typography>
+          </Box>
+        ))}
       </Box>
     </div>
   );
